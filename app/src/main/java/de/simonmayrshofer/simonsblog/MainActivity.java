46b2d519139495fede5b082400c55cecabfe43e9
g-mac <simonmayrshofer@hotmail.com>
2016-08-09 21:54:57 +0200

    changed MainActivity to hold ArticlesFragment and make REST call to retrieve articles

--- app/src/main/java/de/simonmayrshofer/simonsblog/MainActivity.java
+++ app/src/main/java/de/simonmayrshofer/simonsblog/MainActivity.java
@@ -1,13 +1,36 @@
 package de.simonmayrshofer.simonsblog;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    ArticlesFragment articlesFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        articlesFragment = new ArticlesFragment();
+        replaceFragment(articlesFragment);
+
+        setSupportActionBar(toolbar);
+    }
+
+    public void replaceFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.main_activity_content_view, fragment)
+                .commit();
     }
 }
