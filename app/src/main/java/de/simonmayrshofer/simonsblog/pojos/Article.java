32bb5771660eb033766bfbc83a7923e0b1e5a15f
g-mac <simonmayrshofer@hotmail.com>
2016-08-23 23:54:40 +0200

    made Article pojo an Active Android table

--- app/src/main/java/de/simonmayrshofer/simonsblog/pojos/Article.java
+++ app/src/main/java/de/simonmayrshofer/simonsblog/pojos/Article.java
@@ -1,14 +1,23 @@
 package de.simonmayrshofer.simonsblog.pojos;
 
+import com.activeandroid.Model;
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
 import com.google.gson.annotations.SerializedName;
 
-public class Article {
+@Table(name = "Article")
+public class Article extends Model {
 
+    @Column (name = "backend_id")
     public Integer id;
+    @Column
     public String title;
+    @Column
     public String text;
+    @Column
     @SerializedName("created_at")
     public String createdAt;
+    @Column
     @SerializedName("updated_at")
     public String updatedAt;
 
