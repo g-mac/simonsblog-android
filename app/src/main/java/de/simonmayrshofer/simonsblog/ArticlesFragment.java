2581fbc1726192cd012a1ef18f6b622f1917711b
g-mac <simonmayrshofer@hotmail.com>
2016-08-24 13:11:45 +0200

    implement OnClick to open ArticleFragment for list item to view Article Details

--- app/src/main/java/de/simonmayrshofer/simonsblog/ArticlesFragment.java
+++ app/src/main/java/de/simonmayrshofer/simonsblog/ArticlesFragment.java
@@ -7,8 +7,12 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import com.activeandroid.query.Delete;
+import com.activeandroid.query.Select;
 
 import java.util.List;
 
@@ -34,12 +38,27 @@ public class ArticlesFragment extends Fragment {
     private String userEmail;
     private String userToken;
 
+    private List<Article> articles;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_articles, container, false);
         ButterKnife.bind(this, view);
 
         apiManager = new APIManager();
+        resultsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                int articleId = ((Article) resultsListView.getAdapter().getItem(i)).id;
+                openArticleFragment(articleId);
+            }
+        });
+
+        articles = loadArticles();
+        if (articles.size() > 0)
+            displayResults(articles);
+
+        onGetArticlesClick(null);
 
         return view;
     }
@@ -51,9 +70,10 @@ public class ArticlesFragment extends Fragment {
         resultsView.setVisibility(View.VISIBLE);
         apiManager.getArticles(userEmail, userToken)
                 .subscribeOn(Schedulers.io()) // need to run network call on another bg thread
-//                .doOnNext(hotspots -> saveData(hotspots)) //save data on bg thread
+                .doOnNext(articles -> saveData(articles)) //save data on bg thread
                 .observeOn(AndroidSchedulers.mainThread()) // run onSuccess on UI thread
                 .subscribe(articles -> {
+                    this.articles = articles;
                     displayResults(articles);
                 }, throwable -> {
                     resultsView.setText("API CALL FAILURE: " + throwable.toString());
@@ -96,10 +116,55 @@ public class ArticlesFragment extends Fragment {
                 });
     }
 
+    //----------------------------------------------------------------------------------------------s
+
+    private void displayResults(List<Article> articles) {
+        resultsView.setVisibility(View.GONE);
+        resultsListView.setAdapter(new ArticlesListAdapter(getActivity(), articles));
+    }
+
     //----------------------------------------------------------------------------------------------
 
-    private void displayResults(List<Article> results) {
-        resultsView.setVisibility(View.GONE);
-        resultsListView.setAdapter(new ArticlesListAdapter(getActivity(), results));
+    public static List<Article> loadArticles() {
+        return new Select()
+                .from(Article.class)
+                .execute();
     }
+
+    private void saveData(List<Article> articles) {
+
+//        Log.d("SIMON", "articles.size(): " + articles.size());
+//        Log.d("SIMON", "articles.get(0).getId: " + articles.get(0).id);
+
+        new Delete().from(Article.class).execute(); // delete all existing records
+
+        for (Article article : articles) {
+            article.save();
+        }
+
+        int savedArticles = (new Select()
+                .from(Article.class)
+                .execute()).size();
+
+        Log.d("SIMON", "savedArticles: " + savedArticles);
+
+
+//        for (Hotspot hotspot : hotspots) {
+//            hotspot.save();
+//            Position position = hotspot.getPosition();
+//            position.hotspot = hotspot;
+//            position.save();
+//            for (Translation translation : hotspot.getTranslations()) {
+//                translation.hotspot = hotspot;
+//                translation.save();
+//            }
+//        }
+    }
+
+    //----------------------------------------------------------------------------------------------
+
+    private void openArticleFragment(int articleId) {
+        ((MainActivity) getActivity()).replaceFragment(ArticleFragment.newInstance(articleId));
+    }
+
 }
